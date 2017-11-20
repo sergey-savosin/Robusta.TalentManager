acad58af338790a64498746f0f5c4650b3b34477
Sergey-Savosin <savosin_sergey@mail.ru>
2017-11-20 22:58:58 +0300

    connectionString, EmployeesController

---
+++ Rousta.TalentManager.WebApi.WebHost/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Robusta.TalentManager.Data;
+using Robusta.TalentManager.Domain;
+using Robusta.TalentManager.WebApi.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Rousta.TalentManager.WebApi.WebHost.Controllers
+{
+    public class EmployeesController: ApiController
+    {
+        private readonly IUnitOfWork uow = null;
+        private readonly IRepository<Employee> repository = null;
+        private readonly IMapper mapper = null;
+
+        public EmployeesController(IUnitOfWork uow, IRepository<Employee> repository, IMapper mapper)
+        {
+            this.uow = uow;
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            var employee = repository.Find(id);
+            if (employee == null)
+            {
+                var response = Request.CreateResponse(HttpStatusCode.NotFound, "Employee not found");
+
+                throw new HttpResponseException(response);
+            }
+
+            return Request.CreateResponse<EmployeeDto>(HttpStatusCode.OK, mapper.Map<Employee, EmployeeDto>(employee));
+        }
+    }
+
+}
